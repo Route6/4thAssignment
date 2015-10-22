7cb17549473f7a0ce4fbf8b2b0828898c9e6ac75
John McGlinn <johnmcglinn@gmail.com>
2015-10-22 17:38:33 -0400

    First Commit.

---
+++ 4thAssignment.js
@@ -0,0 +1,14 @@
+var DataTypes = ["1", "Number", "True","9.0"];
+console.log(DataTypes[2]);
+
+/* Is this where I can comment out code?
+var myBeer = {style: "Lager", abv: "Strong"};
+var cipher = "abv";
+console.log(myBeer[cipher]);
+
+alert("Pop it up");
+
+//console.log("Is my JS file linked?")
+
+var golfClubs = ["Driver", "Wedge", "Putter", "Irons"];
+console.log(golfClubs[1]);
